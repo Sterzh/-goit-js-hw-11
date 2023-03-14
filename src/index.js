04bd11da753e0468aea8d79d086b83c98f36e4fd
Serhii Tomashevskyi <tom5423000@gmail.com>
2023-03-15 00:30:07 +0200

    save

--- src/index.js
+++ src/index.js
@@ -8,55 +8,71 @@ const refs = {
   searchForm: document.querySelector('.search-form'),
   input: document.querySelector('input[name="searchQuery"]'),
   gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
 };
 
-console.log(refs.gallery);
+console.log(refs.loadMore);
 
 refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
 
   const requestValue = refs.input.value;
 
+  API.resetPage();
+
   API.fetchValue(requestValue)
     .then(value => {
       // console.log(value);
-      render(value);
+      render(value.hits);
     })
     .catch(error => {
       console.log(error);
-      //   onFetchError(error);
+    });
+}
+
+function onLoadMore(e) {
+  const requestValue = refs.input.value;
+  API.fetchValue(requestValue)
+    .then(value => {
+      // console.log(value);
+      render(value.hits);
+    })
+    .catch(error => {
+      console.log(error);
     });
 }
 
 function render(value) {
   console.log(value);
+  if (value === []) {
+    Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    const markup = value
+      .map(
+        ({
+          tags,
+          webformatURL,
+          largeImageURL,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<div class="gallery__card"><a class="gallery__link" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a><div class="card-info"><p class="info-item"><b>Likes<br>${likes}</b></p><p class="info-item"><b>Views<br>${views}</b></p><p class="info-item"><b>Comments<br>${comments}</b></p><p class="info-item"><b>Downloads<br>${downloads}</b></p></div></div>`;
+        }
+      )
+      .join('');
 
-  const markup = value
-    .map(
-      ({
-        tags,
-        webformatURL,
-        largeImageURL,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="gallery__card"><a class="gallery__link" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a><div class="card-info"><p class="info-item"><b>Likes ${likes}</b></p><p class="info-item"><b>Views ${views}</b></p><p class="info-item"><b>Comments ${comments}</b></p><p class="info-item"><b>Downloads ${downloads}</b></p></div></div>`;
-      }
-    )
-    .join('');
+    refs.gallery.innerHTML = markup;
 
-  refs.gallery.innerHTML = markup;
-
-  const litebox = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-    captionsData: 'alt',
-  });
+    const litebox = new SimpleLightbox('.gallery a', {
+      captionDelay: 250,
+      captionsData: 'alt',
+    });
+  }
 }
-
-// function onFetchError(error) {
-//   Notiflix.Notify.failure('Oops, there is no country with that name');
-// }
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,18 +1,25 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const MY_API_PIXABAY_KEY = '?key=34337026-7de7d7fed724711432526467d';
 
+let page = 1;
+
 function fetchValue(requestValue) {
   return fetch(
-    `${BASE_URL}${MY_API_PIXABAY_KEY}&q=${requestValue}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${BASE_URL}${MY_API_PIXABAY_KEY}&q=${requestValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   ).then(r => {
     return r.json().then(data => {
-      return data.hits;
+      incrementPage();
+      return data;
     });
   });
 }
 
-export default { fetchValue };
+function incrementPage() {
+  page += 1;
+}
 
-// resetPage() {
-//   page = 1
-// }
+function resetPage() {
+  page = 1;
+}
+
+export default { fetchValue, resetPage };
